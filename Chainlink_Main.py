75fc20b49c4a03b191fc298da5a109ffc7338ed7
Randy G <rjgriggs0072@gmail.com>
2024-08-01 20:32:12 -0700

    Initial Commit

--- Chainlink_Main.py
+++ Chainlink_Main.py
@@ -1,7 +1,7 @@
 
 import streamlit as st
 from login import  login  #login_form
-import snowflake_connection
+from snowflake_connection import get_snowflake_connection
 import pandas as pd
 import bcrypt
 import uuid
